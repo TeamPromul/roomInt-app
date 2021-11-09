79679a11ad3683e9f66521fe589f959092116aea
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2021-11-09 10:14:53 +0700

    Initial Commit

---
+++ roomInt-app/roomInt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomInt_appApp.swift
+//  roomInt-app
+//
+//  Created by Dicky Buwono on 09/11/21.
+//
+
+import SwiftUI
+
+@main
+struct roomInt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
