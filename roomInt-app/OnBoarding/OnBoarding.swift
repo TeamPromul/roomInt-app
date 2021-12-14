c1ff699a02bdac53cb3c31b18dd68b1d894d1412
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-12-14 07:48:16 +0700

    refactor: add viewmodel and navigationlink

--- roomInt-app/OnBoarding/OnBoarding.swift
+++ roomInt-app/OnBoarding/OnBoarding.swift
@@ -9,82 +9,79 @@ import SwiftUI
 
 struct OnBoarding: View {
     @State var index = 0
-
-    private let textTitle = [
-        "Create your dream room on your hand!",
-        "Find your favorite design architecture",
-        "Discuss with expert in design architecture"
-    ]
+    @State private var isActive = false
+    @StateObject var viewModel =  OnBoardingViewModel()
 
     var body: some View {
-        GeometryReader { proxy in
-            let contentSize = proxy.size.width - 36
-            ZStack {
-                Color.primaryColor
-                VStack(spacing: 0) {
-                    Group {
-                        Image("Illustration\(index + 1)")
-                            .resizable()
-                            .frame(width: 345, height: 309, alignment: .center)
+        NavigationView {
+            GeometryReader { proxy in
+                let contentSize = proxy.size.width - 36
+                ZStack {
+                    Color.primaryColor
+                    VStack(spacing: 0) {
                         Group {
-                            Text(textTitle[index])
-                                .textH3Bold()
-                                .padding(.top, 18)
-                            Text("Lorem ipsun dolor sit amet, consectetur adipiscing elit. aenean eros diam massa sit euis mod aligquet nibh tortor city people.")
-                                .textH5Regular()
-                                .padding(.top, 16)
+                            Image("Illustration\(index + 1)")
+                                .resizable()
+                                .frame(width: 345, height: 309, alignment: .center)
+                            Group {
+                                Text(viewModel.textTitle[index])
+                                    .textH3Bold()
+                                    .padding(.top, 18)
+                                Text("Lorem ipsun dolor sit amet, consectetur adipiscing elit. aenean eros diam massa sit euis mod aligquet nibh tortor city people.")
+                                    .textH5Regular()
+                                    .padding(.top, 16)
+                            }
+                            .frame(maxWidth: contentSize, minHeight: 20, alignment: .leading)
                         }
-                        .frame(maxWidth: contentSize, minHeight: 20, alignment: .leading)
-                    }
 
-                    HStack {
-                        Capsule()
-                            .frame(width: (index == 0) ? 20: 8, height: 8)
-                            .foregroundColor(Color.white)
-                        Capsule()
-                            .frame(width: (index == 1) ? 20: 8, height: 8)
-                            .foregroundColor(Color.white)
-                        Capsule()
-                            .frame(width: (index == 2) ? 20: 8, height: 8)
-                            .foregroundColor(Color.white)
-                    }.padding()
+                        HStack {
+                            Capsule()
+                                .frame(width: (index == 0) ? 20: 8, height: 8)
+                                .foregroundColor(Color.white)
+                            Capsule()
+                                .frame(width: (index == 1) ? 20: 8, height: 8)
+                                .foregroundColor(Color.white)
+                            Capsule()
+                                .frame(width: (index == 2) ? 20: 8, height: 8)
+                                .foregroundColor(Color.white)
+                        }.padding()
 
-                    HStack(spacing: 16) {
-                        if index < 2 {
-                            Button(action: {
-                                index = 2
-                            }) {
-                                Text("Skip")
-                                    .padding(.horizontal, 46)
-                            }.buttonStyle(SecondaryButtonStyle())
+                        HStack(spacing: 16) {
+                            if index < 2 {
+                                Button(action: {
+                                    index = 2
+                                }) {
+                                    Text("Skip")
+                                        .padding(.horizontal, 46)
+                                }.buttonStyle(SecondaryButtonStyle())
 
-                            Button(action: {
-                                if index < 2 {
-                                    index += 1
-                                    print(index)
+                                Button(action: {
+                                    if index < 2 {
+                                        index += 1
+                                        print(index)
+                                    }
+                                }) {
+                                    Text("Next")
+                                        .padding(.horizontal, 46)
+                                }.buttonStyle(PrimaryButtonStyle())
+                            } else {
+                                NavigationLink(destination: ContentView(), isActive: $isActive) {
+                                    Button(action: {
+                                        isActive.toggle()
+                                        viewModel.onboarding = true
+                                    }) {
+                                        Text("Get Started")
+                                            .frame(maxWidth: contentSize)
+                                    }.buttonStyle(PrimaryButtonStyle())
                                 }
-                            }) {
-                                Text("Next")
-                                    .padding(.horizontal, 46)
-                            }.buttonStyle(PrimaryButtonStyle())
-                        } else {
-                            Button(action: {
-                                
-                            }) {
-                                Text("Get Started")
-                                    .frame(maxWidth: contentSize)
-                            }.buttonStyle(PrimaryButtonStyle())
-                        }
 
+                            }
+                        }.padding(.top, 40)
+                    }
+                    .padding(.vertical, 90)
+                }.ignoresSafeArea()
 
-
-
-                    }.padding(.top, 40)
-                }
-                .frame(maxWidth: contentSize, maxHeight: proxy.size.height)
-                .padding(.vertical, 90)
-            }.ignoresSafeArea()
-
+            }
         }
 
     }
@@ -93,6 +90,6 @@ struct OnBoarding: View {
 struct OnBoarding_Previews: PreviewProvider {
     static var previews: some View {
         OnBoarding(index: 0)
-            .overlay(SplashScreen())
+//            .overlay(SplashScreen())
     }
 }
