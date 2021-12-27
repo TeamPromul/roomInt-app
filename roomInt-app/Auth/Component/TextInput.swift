5506ebb7443aca6a27af4aecb1ad8d2a9c6ab317
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-12-28 00:37:27 +0700

    feat: create reusable component for textfield

---
+++ roomInt-app/Auth/Component/TextInput.swift
@@ -0,0 +1,41 @@
+//
+//  TextInput.swift
+//  roomInt-app
+//
+//  Created by Muhammad Rifki Widadi on 27/12/21.
+//
+
+import SwiftUI
+
+struct TextInput: View {
+    @Binding var text: String
+    let placeholder: String
+    let keyboardType: UIKeyboardType
+    @State var isPass = false
+    
+
+
+    var body: some View {
+        Group {
+            if isPass {
+                SecureField(placeholder, text: $text)
+
+            } else {
+                TextField(placeholder, text: $text)
+            }
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 40)
+                .strokeBorder(Color.primaryColor, lineWidth: 2)
+        )
+        .background(RoundedRectangle(cornerRadius: 40).fill(Color.white))
+    }
+}
+
+struct TextInput_Previews: PreviewProvider {
+    static var previews: some View {
+        TextInput(text: .constant(""), placeholder: "Nama", keyboardType: .default, isPass: true)
+            .previewLayout(.sizeThatFits)
+    }
+}
