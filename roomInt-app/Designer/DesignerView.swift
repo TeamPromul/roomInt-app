66df28b62e8913385b447d41624dd941c6539af9
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2022-01-03 12:19:38 +0700

    DesignerViewUpdate UI

--- roomInt-app/Designer/DesignerView.swift
+++ roomInt-app/Designer/DesignerView.swift
@@ -9,28 +9,41 @@ import SwiftUI
 
 struct DesignerView: View {
     @State private var navBarTitle = "Find Your Design"
+    @State var showNewView = false
     var body: some View {
         NavigationView {
-                  TabView {
-                      DashboardTabItem()
-                          .tabItem {
-                              Image(systemName: "house")
-                              Text("Home")
-                          }
-                      
-                      ConsultDesignTabItem()
-                          .tabItem {
-                              Image(systemName: "ellipsis.bubble")
-                              Text("Consult")
-                          }
-                      
-                      ProfileDesignTabItem()
-                          .tabItem {
-                              Image(systemName: "person.circle")
-                              Text("Profile")
-                          }
-                  }
-              }
+            TabView {
+                DashboardTabItem()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                ConsultDesignTabItem()
+                    .tabItem {
+                        Image(systemName: "ellipsis.bubble")
+                        Text("Consult")
+                    }
+                
+                ProfileDesignTabItem()
+                    .tabItem {
+                        Image(systemName: "person.circle")
+                        Text("Profile")
+                    }
+            }
+            .navigationTitle("Designer")
+            .navigationBarItems(trailing:
+                                    NavigationLink(destination: UploadInteriorView(), isActive: $showNewView) {
+                Button {
+                    self.showNewView = true
+                }label: {
+                    Image(systemName: "plus.app.fill")
+                        .font(.system(size: 25))
+                }
+                
+            })
+            
+        }
     }
 }
 
--- roomInt-app/Designer/Views/UploadInteriorView.swift
+++ roomInt-app/Designer/Views/UploadInteriorView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct UploadInteriorView: View {
     @State var title = ""
     @State var category = Category.livingRoom.rawValue
+    @State var price = ""
     var body: some View {
         VStack(alignment: .center) {
             Form {
                 Section(header: Text("Design Interior")) {
                     TextField("Title",text: $title)
+                    TextField("Price", text: $price)
                     Picker(selection: $category, label: Text("Select Category")) {
                         ForEach(Category.allCases, id: \.id) { categories in
                             Text(categories.rawValue.capitalized)
