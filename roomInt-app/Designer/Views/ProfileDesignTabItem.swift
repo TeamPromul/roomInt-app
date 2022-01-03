c827b520a575fc9a855c2bc2a686e109479ce3df
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2022-01-03 12:44:50 +0700

    Designer View

--- roomInt-app/Designer/Views/ProfileDesignTabItem.swift
+++ roomInt-app/Designer/Views/ProfileDesignTabItem.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ProfileDesignTabItem: View {
+    var profileItem = ["Edit Name", "Shipping Info", "Notification", "Terms & Condition"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            Bubble()
+                .padding(.horizontal)
+            ForEach(profileItem, id: \.self) { item in
+                    HStack {
+                        Text(item)
+                            .font(.system(size: 15, weight: .regular))
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .font(.system(size: 15, weight: .bold))
+                    }.padding()
+                }
+            Button {
+                
+            }label: {
+                Text("Logout")
+                    .font(.system(size: 15, weight: .regular))
+                    .foregroundColor(.red)
+            }
+            .padding(.horizontal)
+            Spacer()
+        }.padding()
     }
 }
 
