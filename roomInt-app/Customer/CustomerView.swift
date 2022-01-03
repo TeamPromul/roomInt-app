be699adaf0d120eb40c15cd88de8513322a029f8
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-03 20:58:07 +0700

    feat: pull changes from dev

--- roomInt-app/Customer/CustomerView.swift
+++ roomInt-app/Customer/CustomerView.swift
@@ -10,39 +10,6 @@ import SwiftUI
 struct CustomerView: View {
     @State private var navBarTitle = "Find Your Design"
     var body: some View {
-<<<<<<< HEAD
-
-        TabView {
-            HomeTabItem(navBarHidden: $navBarHidden, navBarTitle: $navBarTitle, displayMode: $displayMode)
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
-                }
-                .navigationBarTitle(navBarTitle, displayMode: displayMode)
-                .navigationBarHidden(navBarHidden)
-            CatalogTabItem(navBarHidden: $navBarHidden, navBarTitle: $navBarTitle)
-                .tabItem {
-                    Image(systemName: "book")
-                    Text("Catalog")
-                }
-                .navigationBarTitle(navBarTitle, displayMode: displayMode)
-                .navigationBarHidden(navBarHidden)
-            ConsultTabItem(navBarTitle: $navBarTitle, displayMode: $displayMode)
-                .tabItem {
-                    Image(systemName: "ellipsis.bubble")
-                    Text("Consult")
-                }
-                .navigationBarTitle(navBarTitle, displayMode: displayMode)
-            ProfileTabItem(navBarTitle: $navBarTitle, displayMode: $displayMode)
-                .tabItem {
-                    Image(systemName: "person.circle")
-                    Text("Profile")
-                }
-                .navigationBarTitle(navBarTitle, displayMode: displayMode)
-        }
-        .navigationBarBackButtonHidden(true)
-
-=======
         NavigationView {
                   TabView {
                       HomeTabItem(navBarTitle: $navBarTitle)
@@ -71,7 +38,6 @@ struct CustomerView: View {
                           .navigationBarTitle(navBarTitle, displayMode: .automatic)
                   }
               }
->>>>>>> f6e6999ec530eb9f84816ebd0810087761d30598
     }
 }
 
